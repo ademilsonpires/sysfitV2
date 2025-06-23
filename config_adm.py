50367a26fd795584239be92f0ac2cc9eac6d8c1a
ADEMILSON PIRES MIRANDA SÁ <89095980+ademilsonpires@users.noreply.github.com>
2025-06-23 06:35:17 -0300

    1

---
+++ config_adm.py
@@ -0,0 +1,19 @@
+# Configurações do banco de dados
+#variaveis ambiente dev
+# DB_USER = "root"
+# DB_PASSWORD = ""
+# DB_HOST = "localhost"
+# DB_NAME = "sysfit_adm"
+#variaveis ambiente producao
+DB_USER = "slzdominios_webmoney"
+DB_PASSWORD = "VYcanismajoris1#"
+DB_HOST = "31.220.52.31"
+DB_NAME = "slzdominios_sysfit_adm"
+
+
+# Configurações fixas de acesso ao MySQL e dump
+MYSQL_USER = "slzdominios_webmoney"
+MYSQL_PASSWORD = "PQHxium1VewsgtQT"
+MYSQL_HOST = "31.220.52.31"
+MYSQL_PORT = 3306
+DUMP_FILE_PATH = "sysfit_cli_template.sql"  # arquivo dump no mesmo diretório do script
